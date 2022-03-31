ba764820ef923ce4dcd0e910a0bd79b8ba4b8a96
Vinicius Gaio <vinagamer9@gmail.com>
2022-03-31 15:38:54 -0300

    Improved Screen Chessboard

--- Program.cs
+++ Program.cs
@@ -11,11 +11,18 @@ namespace Chess
         {
             try
             {
-                ChessPosition pos = new ChessPosition('c', 7);
+                Chessb chessboard = new Chessb(8, 8);
 
-                Console.WriteLine(pos);
+                chessboard.insertPiece(new Tower(Color.Black, chessboard), new Position(0, 0));
+                chessboard.insertPiece(new Tower(Color.Black, chessboard), new Position(1, 3));
+                chessboard.insertPiece(new King(Color.Black, chessboard), new Position(2, 4));
 
-                Console.WriteLine(pos.toPosition());
+                chessboard.insertPiece(new Tower(Color.White, chessboard), new Position(5, 4));
+                chessboard.insertPiece(new King(Color.White, chessboard), new Position(6, 2));
+
+                Screen.PrintChessboard(chessboard);
+
+                Console.ReadLine();
             }
             catch (ChessboardException e) { Console.WriteLine(e.Message); }
         }
--- Screen.cs
+++ Screen.cs
@@ -9,17 +9,33 @@ namespace Chess
         {
             for (int i = 0; i < chessboard.rows; i++)
             {
+                Console.Write(8 - i + " ");
                 for (int j = 0; j < chessboard.columns; j++)
                 {
                     if (chessboard.piece(i, j) == null)
                     { Console.Write("- "); }
                     else
                     {
-                        Console.Write(chessboard.piece(i, j) + " ");
+                        printPiece(chessboard.piece(i, j));
+                        Console.Write(" ");
                     }
                 }
                 System.Console.WriteLine("");
             }
+            Console.WriteLine("  a b c d e f g h");
+        }
+        public static void printPiece(Piece piece)
+        {
+            if (piece.color == Chessboard.Enums.Color.White)
+            { Console.Write(piece); }
+            else
+            {
+                ConsoleColor aux = Console.ForegroundColor;
+                Console.ForegroundColor = ConsoleColor.DarkYellow;
+                Console.Write(piece);
+                Console.ForegroundColor = aux;
+            }
+
         }
     }
 }
